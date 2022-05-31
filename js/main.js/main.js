0a1bf484e4418d0a6b3de44eda5b4eb984f31b86
maxminelli <chadroussel79@gmail.com>
2022-05-31 17:11:59 -0500

    Last commit

---
+++ js/main.js/main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var myFunc = function myFunc(name, age) {
+  return "Max ${name}, his age is ${age}years old";
+};
+console.log(myFunc("said", 42));
---
+++ source/main.js
@@ -0,0 +1,8 @@
+const myFunc = (name, age) => {
+  return "Max ${name}, his age is ${age}years old";
+};
+console.log(myFunc("said", 42));
+
+// let names = [Max, Minelli, Chad, Roussel];
+
+// let p = [...names];
